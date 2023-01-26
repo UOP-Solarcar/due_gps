cb5b36d6279c222c58f4a09e505ba6d3604e665b
MiguelX413 <lbpgaming27@gmail.com>
2023-01-26 13:51:36 -0800

    Add basic code to rpi/rust_blink

--- rpi/rust_blink/src/main.rs
+++ rpi/rust_blink/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::thread;
+use std::time::Duration;
+
+use rppal::gpio::Gpio;
+
+// Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
+const GPIO_LED: u8 = 17;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
+
+    pin.set_high();
+    loop {
+        thread::sleep(Duration::from_millis(500));
+        pin.set_low();
+        thread::sleep(Duration::from_millis(500));
+        pin.set_high();
+    }
 }
